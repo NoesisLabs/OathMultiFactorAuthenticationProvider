2f5318a02cc008d8b161bc08ac689ae3b77846dd
Ryan J. Melena <Ryan.Melena@noesislabs.com>
2014-05-13 10:19:41 -0500

    First commit

---
+++ src/NoesisLabs.OathMultiFactorAuthenticationProvider/AdapterPresentation.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.IdentityServer.Web.Authentication.External;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoesisLabs.OathMultiFactorAuthenticationProvider
+{
+	public class AdapterPresentation : IAdapterPresentationForm
+	{
+		private string message = String.Empty;
+
+		public AdapterPresentation() { }
+
+		public AdapterPresentation(string message)
+		{
+			this.message = message;
+		}
+
+		/// Returns the HTML Form fragment that contains the adapter user interface. This data will be included in the web page that is presented
+		/// to the cient.
+		public string GetFormHtml(int lcid)
+		{
+			//string htmlTemplate = Resource.Form_en_us;
+
+			string html = "";
+
+			if (!String.IsNullOrEmpty(this.message))
+			{
+				html += "<p><b>" + message + "</b></p>";
+			}
+
+			html += Resources.html;
+
+			return html;
+		}
+
+		/// Return any external resources, ie references to libraries etc., that should be included in 
+		/// the HEAD section of the presentation form html. 
+		public string GetFormPreRenderHtml(int lcid)
+		{
+			return null;
+		}
+
+		//returns the title string for the web page which presents the HTML form content to the end user
+		public string GetPageTitle(int lcid)
+		{
+			return "OATH Multi Factor Authentication";
+		}
+	}
+}
---
+++ src/NoesisLabs.OathMultiFactorAuthenticationProvider/AuthenticationAdapter.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.IdentityServer.Web.Authentication.External;
+using OtpSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.Linq;
+using System.Net;
+using System.Runtime.Caching;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoesisLabs.OathMultiFactorAuthenticationProvider
+{
+	public class AuthenticationAdapter : IAuthenticationAdapter
+	{
+		private const string TOKEN_FORM_FIELD_NAME = "Token";
+
+		private static Dictionary<string, DateTime> usedKeys = new Dictionary<string, DateTime>();
+
+		private ObjectCache cache = MemoryCache.Default;
+
+		private string upn;
+
+		public IAuthenticationAdapterMetadata Metadata
+		{
+			get { return new AuthenticationAdapterMetadata(); }
+		}
+
+		public IAdapterPresentation BeginAuthentication(Claim identityClaim, HttpListenerRequest request, IAuthenticationContext authContext)
+		{
+			this.upn = identityClaim.Value;
+
+			return new AdapterPresentation();
+		}
+
+		public bool IsAvailableForUser(Claim identityClaim, IAuthenticationContext authContext)
+		{
+			string upn = identityClaim.Value;
+			DirectoryEntry entry = new DirectoryEntry();
+			DirectorySearcher mySearcher = new DirectorySearcher(entry, "(&(objectClass=user)(objectCategory=person)(userPrincipalName=" + upn + "))");
+			SearchResult result = mySearcher.FindOne();
+			if (result.Properties["info"].Count == 0) return false;
+			string token = (string)result.Properties["info"][0];
+			return !String.IsNullOrEmpty(token);
+		}
+
+		public void OnAuthenticationPipelineLoad(IAuthenticationMethodConfigData configData)
+		{
+			//this is where AD FS passes us the config data, if such data was supplied at registration of the adapter
+		}
+
+		public void OnAuthenticationPipelineUnload()
+		{
+
+		}
+
+		public IAdapterPresentation OnError(HttpListenerRequest request, ExternalAuthenticationException ex)
+		{
+			//return new instance of IAdapterPresentationForm derived class
+			return new AdapterPresentation();
+		}
+
+		public IAdapterPresentation TryEndAuthentication(IAuthenticationContext authContext, IProofData proofData, HttpListenerRequest request, out Claim[] outgoingClaims)
+		{
+			outgoingClaims = new Claim[0];
+			if (ValidateProofData(proofData, authContext))
+			{
+				//authn complete - return authn method
+				outgoingClaims = new[] 
+					 {
+					 // Return the required authentication method claim, indicating the particular authentication method used.
+					 new Claim( "http://schemas.microsoft.com/ws/2008/06/identity/claims/authenticationmethod", 
+					 "http://schemas.microsoft.com/ws/2012/12/authmethod/otp" )
+					 };
+
+				return null;
+			}
+			else
+			{
+				//authn not complete - return new instance of IAdapterPresentationForm derived class
+				return new AdapterPresentation(Resources.TokenValidationFailedMessage);
+			}
+		}
+
+		private bool ValidateProofData(IProofData proofData, IAuthenticationContext authContext)
+		{
+			if (proofData == null || proofData.Properties == null || !proofData.Properties.ContainsKey(TOKEN_FORM_FIELD_NAME))
+			{
+				throw new ExternalAuthenticationException(Resources.TokenNotFoundMessage, authContext);
+			}
+
+			string key = GetEncodedSecretKey(this.upn);
+
+			Totp otp = new Totp(Base32.Base32Encoder.Decode(key));
+
+			long step;
+
+			bool isVerified = otp.VerifyTotp((string)proofData.Properties[TOKEN_FORM_FIELD_NAME], out step, new VerificationWindow(previous: 1, future: 1));
+
+			if (!isVerified) { return false; }
+
+			string cacheKey = this.upn + "_" + step.ToString();
+
+			if (this.cache.Get(cacheKey) != null)
+			{
+				throw new ExternalAuthenticationException(String.Format(Resources.TokenAlreadyUsedMessage, "[" + this.upn + "] [" + step.ToString() + "]"), authContext);
+			}
+			else
+			{
+				var policy = new CacheItemPolicy() { AbsoluteExpiration = DateTime.Now.Add(new TimeSpan(0, 1, 0)) };
+				this.cache.AddOrGetExisting(new CacheItem(cacheKey, "used"), policy);
+			}
+
+			return true;
+		}
+
+		private static string GetEncodedSecretKey(string upn)
+		{
+			DirectoryEntry entry = new DirectoryEntry();
+			DirectorySearcher mySearcher = new DirectorySearcher(entry, "(&(objectClass=user)(objectCategory=person)(userPrincipalName=" + upn + "))");
+			SearchResult result = mySearcher.FindOne();
+			string deviceId = (string)result.Properties["info"][0];
+			return deviceId;
+		}
+	}
+}
